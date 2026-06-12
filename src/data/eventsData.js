419b7e8cfe6dc2665a9dd0942870c35b8989790d
sim31906 <sim31906@gmail.com>
2026-06-12 22:58:41 +0700

    นา

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -732,7 +732,7 @@ export const eventsData = [
     image: '/events/e-2569-003.jpg',
     links: [
       { platform: 'Facebook', label: 'Facebook', url: 'https://www.facebook.com/CentralChiangmaiAirport/posts/%E0%B8%8A%E0%B8%B2%E0%B8%A7%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88%E0%B9%80%E0%B8%95%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%A1%E0%B8%95%E0%B8%B1%E0%B8%A7%E0%B9%83%E0%B8%AB%E0%B9%89%E0%B8%9E%E0%B8%A3%E0%B9%89%E0%B8%AD%E0%B8%A1-%E0%B8%A1%E0%B8%AB%E0%B8%81%E0%B8%A3%E0%B8%A3%E0%B8%A1%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B9%81%E0%B8%A5%E0%B8%B0%E0%B8%9A%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%97%E0%B8%B4%E0%B8%87%E0%B8%AA%E0%B8%B8%E0%B8%94%E0%B8%A2%E0%B8%B4%E0%B9%88%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%8D%E0%B9%88%E0%B8%81%E0%B8%B3%E0%B8%A5%E0%B8%B1%E0%B8%87%E0%B8%88%E0%B8%B0%E0%B8%A1%E0%B8%B2%E0%B8%96%E0%B8%B6%E0%B8%87%E0%B9%81%E0%B8%A5/1012940434715133/', color: '#1877F2' },
-      { platform: 'Google Drive', label: 'รูปภาพ', url: 'https://drive.google.com/drive/folders/1i2Le-u1yrbu5oo6afFJH-7DJgYbMC0XG', color: '#fff' },
+      { platform: 'Google Drive', label: 'Drive', url: 'https://drive.google.com/drive/folders/1i2Le-u1yrbu5oo6afFJH-7DJgYbMC0XG', color: '#fff' },
     ],
   },
 ];
